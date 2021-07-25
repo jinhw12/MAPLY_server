e436accdd5eb6f510cc4af1945b5f947a941feaa
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-07-25 13:35:50 +0900

    deleteVideo 작성 완료

--- controllers/playlist/deletePlaylist.js
+++ controllers/playlist/deletePlaylist.js
@@ -4,12 +4,11 @@ module.exports = async (req, res) => {
   const { playlist_id } = req.params;
 
   if (!playlist_id) {
-    return res.status(403).send("Invalid playlist ID")
+    res.status(400).send("Invalid playlist ID")
   } else {
-    const deletedPlaylist = await playlist.destroy({
+    await playlist.destroy({
       where: { id: playlist_id }
-    })
-
-    res.send(`Deleted successfully. Deleted playlist : { id : ${deletedPlaylist} }`)
+    });
+    res.send("Deleted successfully");
   }
 };
