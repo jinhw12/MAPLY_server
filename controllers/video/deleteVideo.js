013a6e777e4fba72dd1f8fdbe17eba178edd055a
jinhw12 <jinhw12@naver.com>
2021-07-27 14:10:57 +0900

    deleteVideo 수정

--- controllers/video/deleteVideo.js
+++ controllers/video/deleteVideo.js
@@ -4,17 +4,19 @@ module.exports = async (req, res) => {
   const { video_id } = req.params;
 
   if (!video_id) {
-    res.status(400).send("Invalid video ID")
+    res.status(400).send("Invalid video ID");
   } else {
     const { dataValues } = await video.findOne({ where: { id: video_id } });
     const playlist_id = dataValues.playlist_id;
-    const count = await playlist.count({ where: { id: playlist_id } });
+    const count = await video.count({ where: { playlist_id } });
+    console.log({ count });
 
-    if (count >= 1) {
-      await playlist.destory({ where: { id: playlist_id } })
+    if (count <= 1) {
+      await playlist.destroy({ where: { id: playlist_id } });
     } else {
       await video.destroy({ where: { id: video_id } });
     }
+
     res.send("Deleted successfully");
   }
 };
